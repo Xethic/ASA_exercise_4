2bd8b31e20b1d40448d0802647e1a9172325f93e
Louisa <Louisaschwed@gmx.de>
2016-07-03 14:00:52 +0200

    new function getneighbor

--- assembly.py
+++ assembly.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def kmers(k, reads):
+def computekmers(k, reads):
 	kmer = dict()
 		
 	for r in reads:
@@ -14,135 +14,36 @@ def kmers(k, reads):
 	return kmer
 
 
-def find(k, kmerlist, longest):
-	print longest
-	s = k
 	
-	if s[1:] == longest.items()[0][0][:3]:
-		longest[s] = s[1:]+longest.items()[0][0][:3]
+def getneighbor(node, kmers, k, currlen):
+	ns = node
+	s1 = node[:k-1]
+	s2 = node[-(k-1):]
+	found1 = False
+	found2 = False
+	#print s1, s2
+	for c in ["A", "C", "G", "T"]:
+		#print s2+c, c+s1
+		if s2+c in kmers:
+			if not found1:
+				ns += c
+				found2 = True
+		if c+s1 in kmers:
+			if not found2:
+				ns = c + ns
+				found2 = True
 	
-	first = k
-	
-	for k2 in kmerlist:
-			
-		s1 = first[1:] + "A"
-		s2 = first[1:] + "C"
-		s3 = first[1:] + "G"
-		s4 = first[1:] + "T"
+	#print ns
+
+	if len(node) == len(ns):
+		return ns
+	else:
+		ns = getneighbor(ns, kmers, k, len(ns))
 		
-		if k2 in longest:
-			s += longest[k2]
-		if k2 == s1:
-			s += "A"
-			first = s1
-			found = True
-		elif k2 == s2:
-			s += "C"
-			first = s2
-			found = True
-		elif k2 == s3:
-			s += "G"
-			first = s3
-			found = True
-		elif k2 == s4:
-			s += "T"
-			first = s4
-			found = True
-		else:
-			found = False
-		
-		if not found:
-			return s
-				
-	
-	'''
-def find(kmerlist):	
-	assemb = []
-	startnode = ""
+	return ns
 	
 
 	
-	for key in kmerlist:
-		s = ""
-		found = True		
-		query = []		
-		start = key
-			 
-		s += start
-		
-		query.append(start)
-		
-		f = 0
-		
-		while len(query) != 0:
-			if not found:
-				if s not in assemb:
-					assemb.append(s)
-				s = query[-1]		
-			
-			first = query[-1][1:]
-			
-					
-			f = 0
-			s1 = first + "A"
-			s2 = first + "C"
-			s3 = first + "G"
-			s4 = first + "T"
-			
-			if s1 in kmerlist:
-				s += "A"
-				f += 1
-				found = True
-				query += [s1]
-			else:
-				found = False
-			if s2 in kmerlist:
-				if f == 0: 
-					s += "C"
-					f += 1
-					found = True
-					query += [s2]
-				else:
-					query += [s2]
-					found = True
-			else:
-				if f == 0:
-					found = False
-			if s3 in kmerlist:
-				if f == 0: 
-					s += "G"
-					f += 1
-					found = True
-					query += [s3]
-				else:
-					query += [s3]
-					found = True
-			else:
-				if f == 0:
-					found = False
-			if s4 in kmerlist:
-				if f == 0: 
-					s += "T"
-					f += 1
-					found = True
-					query += [s4]
-				else:
-					query += [s4]
-					found = True
-			else:
-				if f == 0:
-					found = False
-						
-			query.pop(0)
-			
-	
-		if s not in assemb:		
-			assemb.append(s)	
-	
-
-	return assemb
-	
-	'''
 #main
 
 #first argument is the input file
@@ -159,15 +60,17 @@ with open(str(sys.argv[1])) as f:
 		else:
 			reads.append(line)
 
-k = 4
+k = 6
 
-longest = dict()
 assemb = []
 
-kmerlist = kmers(k, reads)
+kmerlist = computekmers(k, reads)
 print kmerlist
 
-for km in kmerlist:
-	assemb.append(find(km, kmerlist, longest))
-	
-print assemb
+start = kmerlist.items()[0][0]
+print start
+
+finals = getneighbor(start, kmerlist, k, len(start))
+print finals
+#print assembl(start, kmers)
+
