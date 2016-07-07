a4c264ac674ce983864b97e4305145a7e71c8983
Fabian Kern <s9fakern@stud.uni-saarland.de>
2016-07-07 16:22:58 +0200

    Further implemented assembly.

--- assembly.py
+++ assembly.py
@@ -1,72 +1,121 @@
 import sys
+from string import maketrans
+
+alphabet = ['A', 'C', 'G', 'T']
+original = 'ACGT'
+reversecomplement = 'TGCA'
+revtable = maketrans(original, reversecomplement)
+
+
+def reverseComplement(sequence):
+	return sequence.translate(revtable)
+
+
+def addreverse(reads):
+	revreads = []
+	for read in reads:
+		revreads.append(reverseComplement(read))
+	reads.extend(revreads)
 
 
 def computekmers(k, reads):
 	kmer = dict()
-		
+	count = 0
 	for r in reads:
+		count+=1
 		for i in range(len(r)-k+1):
 			if r[i:i+k] in kmer:
 				kmer[r[i:i+k]] += 1
 			else:
 				kmer[r[i:i+k]] = 1
-			
+	print 'Parsed ' + str(count) + ' reads'
 	return kmer
-
-
-#def addreverse(kmerlist):
 	
-def errorcorrect(kmerlist):
 	
-	error = []
+def bestHammingNeighbor(v, cutoff, kmers):
+	bestSeq = v
+	bestCount = cutoff-1
+	for i in range(0, len(v)):
+		for a in alphabet:
+			new = list(v)
+			new[i] = a
+			new = ''.join(new)
+			if(new in kmers):
+				if(kmers[new] > bestCount):
+					bestSeq = new
+					bestCount = kmers[new]
+	return bestSeq
 	
-	for k in kmerlist:
-		if kmerlist[k] == 1:
-			if k not in error:
-				error.append(k)
 	
-	for e in error:
-		del kmerlist[e]
-		
-	return kmerlist
+def errorcorrect(reads, cutoff, k):
+	
+	kmers = computekmers(k, reads)
+	print 'Correcting errors....'
+	for read in reads:
+		for i in range(len(read)-k+1):
+			v = read[i:i+k]
+			if(not v in kmers):
+				kmers[v] = 1
+			if(kmers[v] < cutoff):
+				new = bestHammingNeighbor(v, cutoff, kmers)
+				if new != v:
+					kmers[new] += 1
+					read = read[0:i] + new + read[i+k: len(read)]
+	print 'Finished!'
 			
 	
-		
-	
-def getneighbor(node, kmers, k, currlen):
+def getneighbor(node, kmers, k):
 	ns = node
-	s1 = node[:k-1]
-	s2 = node[-(k-1):]
-	found1 = False
-	found2 = False
-	#print s1, s2
-	for c in ["A", "C", "G", "T"]:
-		#print s2+c, c+s1
-		if s2+c in kmers:
-			if not found1:
-				ns += c
-				found2 = True
-		if c+s1 in kmers:
-			if not found2:
-				ns = c + ns
-				found2 = True
-	
-	#print ns
-
-	if len(node) == len(ns):
-		return ns
-	else:
-		ns = getneighbor(ns, kmers, k, len(ns))
+	converged = False
+	while not converged:
+		s1 = ns[:k-1]
+		s2 = ns[-(k-1):]
+		found1 = False
+		found2 = False
+		oldlength = len(ns)
+		#print s1, s2
+		for c in ["A", "C", "G", "T"]:
+			#print s2+c, c+s1
+			if s2+c in kmers:
+				if not found1:
+					ns += c
+					found1 = True
+			if c+s1 in kmers:
+				if not found2:
+					ns = c + ns
+					found2 = True
+		
+		if oldlength == len(ns):
+			converged = True
 		
 	return ns
-	
 
-	
-#main
 
-#first argument is the input file
-#second argument is the name of the output file
+def countKmersOfCountOne(kmers, k):
+	count = 0
+	for kmer in kmers:
+		if kmers[kmer] == 1:
+			count +=1
+	
+	print 'Found ' + str(count) + ' ' +str(k) + '-mers that were present once' 
+	
+	
+def assemble(kmers, k):
+	finals = set()
+	for kmer in kmers:
+		finals.add(getneighbor(kmer, kmerlist, k))
+	return finals
+
+
+#######
+#	MAIN
+#
+#	first argument is the input file*
+#	second argument is the name of the output file
+#######
+
 reads = []
+readset = set()
 
 with open(str(sys.argv[1])) as f:
 	for line in f:
@@ -76,27 +125,27 @@ with open(str(sys.argv[1])) as f:
 		if line[0] == ">":
 			continue
 		else:
-			reads.append(line)
+			read = line.replace('\n', '')
+			reads.append(read)
+			readset.add(read)
 
-k = 6
-
+k = 21
+errorcutoff = 2
 assemb = []
 
+addreverse(reads)
+
+errorcorrect(reads, errorcutoff, k)
+
 kmerlist = computekmers(k, reads)
-print kmerlist
-
-#kmerlist = addreverse(kmerlist)
-
-kmerlist = errorcorrect(kmerlist)
-print kmerlist
+print 'Generated ' + str(len(kmerlist)) + ' many ' + str(k) + '-mers'
+countKmersOfCountOne(kmerlist, k)
 
 if len(kmerlist) == 0:
 	exit()
 
-start = kmerlist.items()[0][0]
-print start
+finals = assemble(kmerlist, k)
 
-finals = getneighbor(start, kmerlist, k, len(start))
-print finals
-#print assembl(start, kmers)
-
+print str(len(finals))
+finals = list(finals)
+print str(max(finals))
